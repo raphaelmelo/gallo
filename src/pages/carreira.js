126357fd42f14ff319f6b3185860e28fb19eec72
raphaelmelo <contatowbm@gmail.com>
2021-05-01 10:54:10 -0300

    Update depencias

--- src/pages/carreira.js
+++ src/pages/carreira.js
@@ -4,12 +4,16 @@ import Header from '../components/Header/Header.js'
 
 const Title = styled.h1`
   font-size: 50px;
+  background:red;
+  width:100%;
+  height:200px;
 `
 
 export default function Carreira() {
   return (
     <React.Fragment>
       <Header />
+      <Title />
     </React.Fragment>
   );
 }
