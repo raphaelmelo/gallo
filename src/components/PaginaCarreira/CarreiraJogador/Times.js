c2043b00e05f56d8c5d3ff5f4a86ff43b476651a
raphaelmelo <contatowbm@gmail.com>
2021-06-03 13:33:58 -0300

    Corrigido erro no map do carreira jogador

--- src/components/PaginaCarreira/CarreiraJogador/Times.js
+++ src/components/PaginaCarreira/CarreiraJogador/Times.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
-import {Jogador} from './Jogador'
+import { DataJogador } from '../../../dataJogador'
 
-    
 
 const Container = styled.section`
     padding:0 10vw;
@@ -28,57 +27,6 @@ const CarreiraWrapper = styled.section`
     flex-wrap: wrap;
     justify-content: space-around;
 `
-
-
-   
-const DataJogador = [
-    {
-        time: "Botafogo de Ribeirão Preto/SP - 1986/1991",
-        Trofeu: ["Campeonato Paulista"," Campeonato Brasileiro"],
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040842/BotafogoFCRB_szwvxw.png",
-    },
-    {
-        time: "Vitória/BA - 1991",
-        Trofeu: "Campeonato Brasileiro",
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040841/Vit%C3%B3ria_qq0sz4.png",
-    },
-{
-        time: "Santos/SP - 1992/1996",
-        Trofeu: ["Campeonato Paulista","Campeonato Brasileiro (Vice-Campeão)"],
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040844/LogoSantosFC_zobpvy.png",
-    },
-    {
-        time: "Portuguesa/SP - 1996",
-        Trofeu: "Campeonato Brasileiro (Vice-Campeão)",
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040844/Portuguesa_e5p2r5.png",
-    },
-    {
-        time: "Guarani/SP - 1997",
-        Trofeu: "Campeonato Paulista",
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040844/Guarani_FC__E__-_SP.svg_fsoymz.png",
-    },
-    {
-        time: "São Paulo/SP - 1997/1998",
-        Trofeu: ["Campeonato Paulista (Campeão)","Campeonato Brasileiro","Copa do Brasil"],
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1622682230/image_65_nsnsrt.png",
-    },
-    {
-        time: "Botafogo/RJ - 1999",
-        Trofeu: ["Campeonato Carioca (Campeão)","Copa do Brasil (Vice-Campeão)"],
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1622682339/image_65_1_qgkcry.png",
-    },
-    {
-        time: "Atletico Mineiro/Mg - 1999/2000",
-        Trofeu: ["Campeonato Mineiro (Campeão)","Campeonato Brasileiro (Vice-Campeão)"],
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040841/1200px-Atletico_mineiro_galo_ly6lgh.png",
-    },
-    {
-        time: "Corinthians/SP - 2001",
-        Trofeu: ["Campeonato Paulista (Campeão)","Campeonato Brasileiro","Copa do Brasil (Vice-Campeão)"],
-        Escudo: "https://res.cloudinary.com/gallo/image/upload/v1617040842/corinthians_adfphu.png",
-    }
-    
-]
 
 
 const CardTime = styled.article`
@@ -128,28 +76,28 @@ const Escudo = styled.img`
 
 const Content = DataJogador.map((item) => (
     (
-       <CardTime>
+        <CardTime>
             <Barra> </Barra>
             <NomeTimeWrapper>
                 <h1>{item.time}</h1>
                 <h2>{item.Trofeu}</h2>
             </NomeTimeWrapper>
             <EscudoWrapper>
-                <Escudo src={item.Escudo} alt={item.time}/>
+                <Escudo src={item.Escudo} alt={item.time} />
             </EscudoWrapper>
         </CardTime>
-)
+    )
 ))
 
 export default function Times() {
     return (
         <Container>
             <Titulo>
-            <h1>Carreira como jogador</h1>
+                <h1>Carreira como jogador</h1>
             </Titulo>
             <CarreiraWrapper>
-                {Content}
-                </CarreiraWrapper>
+                {Content || ""}
+            </CarreiraWrapper>
         </Container>
     );
 }
